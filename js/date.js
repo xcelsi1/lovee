81055dab57fae15ecdda55973802a51789f91629
Wil Helmina <106503631+xcelsi1@users.noreply.github.com>
2022-09-13 07:57:10 +0700

    Update date.js

--- js/date.js
+++ js/date.js
@@ -20,28 +20,4 @@ function updateTimer() {
       '<div>' + m + '<span>minutes</span></div>' +
       '<div>' + s + '<span>seconds</span></div>' ;
 }
-
-function fadein(){
-	if(val < 1){
-		val += 0.025;
-		dv.style.opacity = val;
-	}
-	else{
-		clearInterval(fadeinInterval);
-		if(ok == 2){
-			ok += 1;
-		}
-	}
-}
-
-var fadeInterval;
-var fadeinInterval;
-
-timer();
-setInterval(timer, 1000);
-fadeInterval = setInterval(function(){
-	if(ok == 2){
-		clearInterval(fadeInterval);
-		fadeinInterval = setInterval(fadein, 50);
-	}
-}, 50)
+setInterval('updateTimer()', 1000 );
