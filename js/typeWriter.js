1019268a615be05b55a64d6246003fec57f59b90
Wil Helmina <106503631+xcelsi1@users.noreply.github.com>
2022-09-14 14:48:21 +0700

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
 let text1 = "Hey! Manda Sayang.";
-let text2 = "So I write some code to celebrate our 1st anniversary heheheheh."
+let text2 = "So I write some code to celebrate our 1st anniversary heheheheh." </br> hsisaihaidhadad
 let speed = 110;
 
 function typeWriter(text, para){
